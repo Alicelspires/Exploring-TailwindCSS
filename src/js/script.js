5201c98468a3f4579a7a509a31fb4cb116b49ace
Alice Silva <alicelspires@gmail.com>
2025-03-24 17:52:58 -0300

    Delete src directory

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("menu-btn");
-const navbar = document.getElementById("menu");
-
-btn.addEventListener("click", () =>{
-    btn.classList.toggle("open");
-    navbar.classList.toggle("flex");
-    navbar.classList.toggle("hidden");
-})
